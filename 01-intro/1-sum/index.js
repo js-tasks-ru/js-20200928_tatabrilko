ad87ab7d44e71d6265264139dd8f65f4787fc53a
Tata Brilko <tasya555@mail.ru>
2020-10-05 00:01:15 +0700

    sort Array asc

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -8,6 +8,6 @@ export default function sum(m, n) {
     return m + n;
 }
  let result = sum(1,1);
- alert (result);
+ 
  result = sum(2,5);
- alert (result);
+ 
--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,20 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+	let collator = new Intl.Collator(undefined, {
+	caseFirst: "upper"
+});
+    arr.sort(function(a, b) {
+  return collator.compare(a, b);
+});
 }
+
+
+let arr = ['b', 'a', 'c'];
+ sortStrings(arr, param = 'asc');
+
+ 
+
+ arr = ['абрикос','Абрикос','яблоко','Яблоко','ёжик','Ёжик'];
+ sortStrings(arr, param = 'asc');
+
